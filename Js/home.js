ba7f652ac8d6afc0175e2253477ddf1e54b97c61
Saif-Mahmud-Khan-Dourjoy <saifmahmudkhandourjoy@gmail.com>
2020-02-01 03:24:29 +0600

    first commit

---
+++ Js/home.js
@@ -0,0 +1,15 @@
+
+function changeText(){
+ 
+        document.getElementById("change").value="Enter";
+    }
+      
+    
+    //for auto reload
+    /* setTimeout(function(){
+        location.reload();
+    },3000); */
+
+function previousText(){
+    document.getElementById("change").value="View Profile";
+}
